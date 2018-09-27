2ce2c54dc448ae1edd88f7ee7ba5d23ea7cfcf9b
Sean Newman <sxn6296@rit.edu>
2018-09-27 12:11:20 -0400

    Finish Player model
    All methods and attributes defined in the class model and required for a
    model-tier entity have been implemented.

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -41,25 +41,44 @@ public class Player {
         return this.name;
     }
 
+    /**
+     * Check if the player is currently signed in
+     *
+     * @return
+     *      {@code true} if the player is signed in, {@code false} if the
+     *      player is not signed in
+     */
     public boolean isSignedIn() {
-        return false;
+        return this.sessionID != null;
     }
 
+    /**
+     * Sign in the player and associate a given session ID with the player
+     *
+     * @param sessionID
+     *      The session ID to associate with the player
+     */
     public void signIn(String sessionID) {
-
+        this.sessionID = sessionID;
     }
 
+    /**
+     * Sign out the player.
+     */
     public void signOut() {
-
+        this.sessionID = null;
     }
 
     @Override
     public boolean equals(Object obj) {
-        return false;
+        if (obj == this) return true;
+        if (!(obj instanceof Player)) return false;
+        final Player that = (Player) obj;
+        return this.name.equals(that.name);
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        return this.name.hashCode();
     }
 }
