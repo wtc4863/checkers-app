08991a57846020837887ea2ad50e55eb898c197b
Ryan Cervantes <rxc3202@rit.edu>
2018-10-27 20:02:50 -0400

    fixed path for tests

--- src/test/java/com/webcheckers/model/MoveTest.java
+++ src/test/java/com/webcheckers/model/MoveTest.java
@@ -1,4 +1,4 @@
-package com.webcheckers.com.webcheckers.model;
+package com.webcheckers.model;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -6,6 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
 import org.mockito.Mockito;
-public class PositionTest {
+public class MoveTest {
 
 }
