8e7223aa60393f9f1129873fe481eb9eb5df2c9c
Ryan Cervantes <rxc3202@rit.edu>
2018-10-30 14:49:39 -0400

    stubbed out PostValidateMoveTest

---
+++ src/test/java/com/webcheckers/ui/PostValidateMoveTest.java
@@ -0,0 +1,33 @@
+package com.webcheckers.ui;
+
+import static org.mockito.Mockito.mock;
+
+import com.webcheckers.appl.PlayerLobby;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import spark.Request;
+import spark.Response;
+
+@Tag("UI-Tier")
+public class PostValidateMoveTest {
+
+    // Component Under Test
+    PostValidateMoveRoute CuT;
+
+    // Mocked Objects
+    Request request;
+    Response response;
+    PlayerLobby playerLobby;
+
+    @Test
+    public void testConstructor() {
+
+    }
+
+    @Test
+    public void testHandle() {
+
+    }
+
+
+}
