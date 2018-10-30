2b4f1e2991003feba99b660e48577263109c50f7
Ryan Cervantes <rxc3202@rit.edu>
2018-10-30 11:59:49 -0400

    Added more testing to the PostCheckTurnRoute
    added the test case where if it is not the players turn it should return
    false

--- src/test/java/com/webcheckers/ui/PostCheckTurnRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostCheckTurnRouteTest.java
@@ -3,6 +3,7 @@ package com.webcheckers.ui;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.Game;
@@ -12,7 +13,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
-import org.mockito.Mockito;
 import spark.Request;
 import spark.Response;
 import spark.Session;
@@ -21,12 +21,15 @@ import spark.Session;
 public class PostCheckTurnRouteTest {
 
     private static final String TEST_ID = "1";
-    private static final String IS_PLAYERS_TURN_JSON = "{\"text\":\"true\", \"type\":\"info\"}";
-    private static final String NOT_PLAYERS_TURN_JSON = "{\"text\":\"false\", \"type\":\"info\"}";
+    private static final String IS_PLAYERS_TURN_JSON = "{\"text\":\"true\",\"type\":\"info\"}";
+    private static final String NOT_PLAYERS_TURN_JSON = "{\"text\":\"false\",\"type\":\"info\"}";
 
 
     // Component Under Test
     private PostCheckTurnRoute CuT;
+
+    // Cannot Mock final class
+    private Gson gson;
 
     // Mocked objects
     private PlayerLobby playerLobby;
@@ -35,13 +38,21 @@ public class PostCheckTurnRouteTest {
     private Session session;
     private Request request;
     private Response response;
-    private GsonBuilder GsonBuilder;
 
     @BeforeEach
     public void setup(){
         playerLobby = mock(PlayerLobby.class);
-        CuT = new PostCheckTurnRoute(playerLobby);
-        GsonBuilder = new GsonBuilder();
+        gson = new Gson();
+        session = mock(Session.class);
+        response = mock(Response.class);
+        request = mock(Request.class);
+        player = mock(Player.class);
+        game = mock(Game.class);
+        when(request.session()).thenReturn(session);
+        when(session.id()).thenReturn(TEST_ID);
+        when(playerLobby.getPlayerBySessionID(TEST_ID)).thenReturn(player);
+        when(playerLobby.getGame(player)).thenReturn(game);
+        CuT = new PostCheckTurnRoute(playerLobby, gson);
     }
 
     @AfterEach
@@ -59,20 +70,18 @@ public class PostCheckTurnRouteTest {
 
     @Test
     public void testHandleReturnTrue() {
-        session = mock(Session.class);
-        response = mock(Response.class);
-        request = mock(Request.class);
-        player = mock(Player.class);
-        game = mock(Game.class);
-        when(request.session()).thenReturn(session);
-        when(session.id()).thenReturn(TEST_ID);
-        when(playerLobby.getPlayerBySessionID(TEST_ID)).thenReturn(player);
-        when(playerLobby.getGame(player)).thenReturn(game);
         when(game.isPlayersTurn(player)).thenReturn(true);
-
         Object result = CuT.handle(request, response);
         String JSONResult = (String)result;
         Assertions.assertEquals(IS_PLAYERS_TURN_JSON, JSONResult);
+    }
+
+    @Test
+    public void testHandleShouldReturnFalse() {
+        when(game.isPlayersTurn(player)).thenReturn(false);
+        Object result = CuT.handle(request, response);
+        String JSONResult = (String)result;
+        Assertions.assertEquals(NOT_PLAYERS_TURN_JSON, JSONResult);
     }
 
 
