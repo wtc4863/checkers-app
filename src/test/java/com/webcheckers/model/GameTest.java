434a573eed8692542597ea6ea669ebcedaedbce4
Ryan Cervantes <rxc3202@rit.edu>
2018-10-30 12:22:54 -0400

    Stubbed out test methods

---
+++ src/test/java/com/webcheckers/model/GameTest.java
@@ -0,0 +1,65 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("Model-tier")
+public class GameTest {
+
+    // Component Under Test
+    private Game CuT;
+
+
+    @Test
+    public void testRegularConstructor() {
+
+    }
+
+    @Test
+    public void testCustomConfigConstructor() {
+
+    }
+
+    @Test
+    public void testGetRedPlayer() {
+
+    }
+
+    @Test
+    public void testGetWhitePlayer() {
+
+    }
+
+    @Test
+    public void testIsPlayersTurn() {
+
+    }
+
+    @Test
+    public void testGetBoar() {
+
+    }
+
+    @Test
+    public void testGetBoardView() {
+
+    }
+
+    @Test
+    public void testGetTurn() {
+
+    }
+
+    @Test
+    public void testSwitchTurn() {
+
+    }
+
+    @Test
+    public void testApplyTurnMoves() {
+
+    }
+
+
+
+}
