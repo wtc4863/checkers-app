ac31d221e6d3cc4dad4d0bab0f7b1d74975856a4
Sean Newman <sxn6296@rit.edu>
2018-10-30 05:11:56 -0400

    Implement JumpMove validateMove & executeMove

--- src/main/java/com/webcheckers/model/JumpMove.java
+++ src/main/java/com/webcheckers/model/JumpMove.java
@@ -14,29 +14,76 @@ public class JumpMove extends Move {
     }
 
     /**
-     * This method will not be used because a move must be a specific subclass of Move itsef.
-     * If a plain Move is used then we throw the UnsupportedOperationException because
-     * a generic move doesn't exist
+     * Check that this move is valid for a given game.
      *
      * @param game the game in which we're moving the piece
-     * @throws UnsupportedOperationException
+     * @return true if the move is valid, false if the move is invalid
      */
     @Override
-    public boolean validateMove(Game game) throws UnsupportedOperationException {
-        return super.validateMove(game);
+    public boolean validateMove(Game game) {
+        // Init variables
+        Piece movedPiece;
+        Piece jumpedPiece;
+
+        // Make sure the spacing is right
+        if(Math.abs(start.getRow() - end.getRow()) != 2 || Math.abs(start.getCell() - end.getCell()) != 2) {
+            return false;
+        }
+
+        // Make sure the starting position has a piece
+        Board board = game.getBoard();
+        if(board.getSpace(start).doesHasPiece()) {
+            // We'll need this information later
+            movedPiece = board.getSpace(start).pieceInfo();
+        } else {
+            return false;
+        }
+
+        // Make sure the ending position doesn't have a piece
+        if(!board.spaceIsValid(end)) {
+            return false;
+        }
+
+        // Make sure the middle position has an opponent piece
+        if(board.getSpace(middle).doesHasPiece()) {
+            jumpedPiece = board.getSpace(middle).pieceInfo();
+            if(jumpedPiece.pieceColor == movedPiece.pieceColor) {
+                // Can only jump an opponent's piece
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        // TODO: skip this block if the piece is a king
+        // Make sure the piece is going in the right direction
+        if(jumpedPiece.pieceColor == Piece.PColor.red) {
+            // Red pieces should travel in the "negative" direction
+            return start.getRow() > end.getRow();
+        } else {
+            // White pieces should travel in the "positive" direction
+            return start.getRow() < end.getRow();
+        }
     }
 
     /**
-     * This method will not be used because a move must be a specific subclass of Move itsef.
-     * If a plain Move is used then we throw the UnsupportedOperationException because
-     * a generic move doesn't exist
+     * Apply this move to a given game
      *
      * @param game the game in which we're moving the piece
-     * @return true if move was made
-     * @throws UnsupportedOperationException
+     * @return true if the move was successfully executed, false otherwise
      */
     @Override
-    public boolean executeMove(Game game) throws UnsupportedOperationException {
-        return super.executeMove(game);
+    public boolean executeMove(Game game) {
+        // Double-check that the move is valid
+        if(validateMove(game)) {
+            Board board = game.getBoard();
+            // Move the piece
+            board.move(start, end);
+
+            // Remove the jumped piece
+            board.getSpace(middle).removePiece();
+        } else {
+            return false;
+        }
     }
 }
