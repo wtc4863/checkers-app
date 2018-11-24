4bfc19edf424467c20db87e10c693c944fd2138f
Sean Newman <sxn6296@rit.edu>
2018-11-23 19:05:51 -0500

    Implement waitingForResponses, finishAsyncRequest
    These are methods in the AsyncServices class that help the GetGameRoute
    component to display the correct message to players when they refresh
    the page.

--- src/main/java/com/webcheckers/appl/AsyncServices.java
+++ src/main/java/com/webcheckers/appl/AsyncServices.java
@@ -4,6 +4,7 @@ import com.webcheckers.model.Game;
 import com.webcheckers.model.Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 
 /**
@@ -59,15 +60,77 @@ public class AsyncServices {
         }
     }
 
+    /**
+     * Confirm the asynchronous play request for the game the player is
+     * currently looking at.
+     *
+     * @param sessionID
+     *      The session ID of the player confirming the transition to
+     *      asynchronous mode.
+     */
     public void confirmAsync(String sessionID) {
         Player confirmingPlayer = playerLobby.getPlayerBySessionID(sessionID);
         Game game = gameCenter.getGame(confirmingPlayer);
         game.acceptAsync();
     }
 
+    /**
+     * Deny the asynchronous play request for the game the player is currently
+     * looking at.
+     *
+     * @param sessionID
+     *      The session ID of the player denying the transition to asynchronous
+     *      mode.
+     */
     public void denyAsync(String sessionID) {
         Player denyingPlayer = playerLobby.getPlayerBySessionID(sessionID);
         Game game = gameCenter.getGame(denyingPlayer);
         game.rejectAsync();
+    }
+
+    /**
+     * Check on an asynchronous request to see how many opponents still need to
+     * respond to the player's request.
+     *
+     * @param player the player making the async request
+     * @return the names of the opponents that still need to respond
+     */
+    public HashMap<String, Game.State> waitingForResponses(Player player) {
+        // Get all of the player's games
+        ArrayList<Game> games = gameCenter.getAllGames(player);
+
+        HashMap<String, Game.State> opponents = new HashMap<>();
+
+        // Check each game state
+        for(Game game : games) {
+            switch(game.getState()) {
+                // ENDED and *ACTIVE states should not be included
+                case ENDED:
+                    break;
+                case ACTIVE:
+                    break;
+                case ASYNC_ACTIVE:
+                    break;
+                default: // All other states represent an async req. response
+                    if (game.isAsyncRequester(player)) {
+                        opponents.put(game.getOpponentOf(player).getName(), game.getState());
+                    }
+            }
+        }
+        return opponents;
+    }
+
+    /**
+     * Finish a player's asynchronous mode request by applying the rejections
+     * or denials from all of their games.
+     *
+     * @param player the player who made an asynchronous mode request
+     */
+    public void finishAsyncRequest(Player player) {
+        for(Game game : gameCenter.getAllGames(player)) {
+            if (game.isAsyncRequester(player)) {
+                game.asyncRequestCompleted();
+            }
+        }
     }
 }
